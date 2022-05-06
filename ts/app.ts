b4473371317fe7a79aab1ef340142e78e4891bfd
AGulev <me@agulev.com>
2022-05-06 14:50:32 +0200

    ux fixes

--- ts/app.ts
+++ ts/app.ts
@@ -11,23 +11,23 @@ function createChart(data: any, marks: any) {
             'task': '#FFFFFF',
             'sub-task': '#000000'
         },
-        settings: {
-            options: {
-                tooltip: () => {/*...*/ }, // see section "Custom Tooltip" below
-                timeUnits: 'ms'
-            },
-            styles: {} // see section "Styles" below
-        }
+        options: {
+            timeUnits: 'ms'
+        },
+        styles: {}
     });
     const nodeView : any = document.getElementById('selected-node');
     flameChart.on('select', (node: any, type: any) => {
         nodeView.innerHTML = (node ? `${type}\r\n${JSON.stringify({
             ...node,
-            source: {
-                ...node.source,
-                children: '[]',
-            },
-            parent: undefined
+            children: undefined,
+            color:undefined,
+            start:undefined,
+            end:undefined,
+            level:undefined,
+            index:undefined,
+            duration: node.duration/1000 +" sec",
+            parent: node.parent ? node.parent.name :undefined
         }, null, '  ')}` : '');
     });
 
@@ -45,10 +45,30 @@ function createChart(data: any, marks: any) {
     canvas.width = width;
     canvas.height = height;
     flameChart.resize(...getWrapperWH());
+    var fullWith = width;
 
     window.addEventListener('resize', () => {
-        flameChart.resize(...getWrapperWH());
+        const [width, height] = getWrapperWH();
+        fullWith = width;
+        flameChart.resize(width, height);
     });
+
+    flameChart.setSettings({ 
+            tooltip : (data : any, renderEngine : any, mouse : any) => {
+                if (!mouse) 
+                    return;
+                var fields = [{ text: data.data.name }, { text: data.data.duration + " ms" }];
+                const maxWidth = fields
+                    .map(({ text }) => text)
+                    .map((text) => renderEngine.ctx.measureText(text))
+                    .reduce((acc, { width }) => Math.max(acc, width), 0);
+                const fullWidth = maxWidth + renderEngine.blockPaddingLeftRight * 2;
+                if (mouse.x + 10 + fullWidth > fullWith) {
+                    mouse.x = mouse.x - 20 - fullWidth;
+                }
+                renderEngine.renderTooltipFromData(fields, mouse);
+            } 
+      });
 }
 
 (window as any).createChart = createChart;
